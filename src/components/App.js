dd3d800bd18c8c7339e5a5af08b5f506c681da82
Vaibhav Singh <77732000+vaibhavsingh2107@users.noreply.github.com>
2022-06-14 12:11:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [flag, setFlag] = useState(true);
   
   return (
     <div id="main">
-
+     <h1 id="marco-polo">{flag ? "Marco" : "Polo"}</h1>
+     <button id="marco-polo-toggler" onClick={setFlag(!flag)}>{flag ? "Polo" : "Marco"}</button>
     </div>
   )
 }
